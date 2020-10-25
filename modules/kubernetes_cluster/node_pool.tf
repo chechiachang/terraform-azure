628416ea78292cde8410cee266b45b8efa00ca8e
Che Chia Chang <chechiachang999@gmail.com>
2020-10-25 16:55:05 +0800

    Add k8s

---
+++ modules/kubernetes_cluster/node_pool.tf
@@ -0,0 +1,34 @@
+resource "azurerm_kubernetes_cluster_node_pool" "main" {
+  for_each              = var.node_pools
+  name                  = each.value.name
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.main.id
+  vm_size               = each.value.vm_size
+  node_count            = each.value.node_count
+  priority              = each.value.priority
+  #eviction_policy       = each.value.eviction_policy
+  #spot_max_price        = each.value.spot_max_price
+  node_labels = each.value.node_labels
+  node_taints = each.value.node_taints
+
+  depends_on = [
+    azurerm_kubernetes_cluster.main
+  ]
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "spot" {
+  for_each              = var.spot_node_pools
+  name                  = each.value.name
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.main.id
+  vm_size               = each.value.vm_size
+  node_count            = each.value.node_count
+  priority              = each.value.priority
+  eviction_policy       = each.value.eviction_policy
+  spot_max_price        = each.value.spot_max_price
+  node_labels           = each.value.node_labels
+  node_taints           = each.value.node_taints
+
+  depends_on = [
+    azurerm_kubernetes_cluster.main
+  ]
+}
+
