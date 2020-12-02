03c0e6dc7bf32c07e54ab38e21c8047b787b9cf4
David Chang <chechiachang999@gmail.com>
2020-12-02 15:33:12 +0800

    FEATURE: add virtual network sg binding

--- chechia/base/virtual_network/terragrunt.hcl
+++ chechia/base/virtual_network/terragrunt.hcl
@@ -12,6 +12,7 @@ inputs = {
     base-external = {
       name              = "base-external"
       address_prefixes  = ["10.0.2.0/24"]
+      network_security_group_id = "/subscriptions/6fce7237-7e8e-4053-8e7d-ecf8a7c392ce/resourceGroups/base/providers/Microsoft.Network/networkSecurityGroups/chechia-home"
     }
   }
 }
--- modules/virtual_network/virtual_network.tf
+++ modules/virtual_network/virtual_network.tf
@@ -12,3 +12,9 @@ resource "azurerm_subnet" "internal" {
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = each.value.address_prefixes
 }
+
+resource "azurerm_subnet_network_security_group_association" "main" {
+  for_each                  = var.subnets
+  subnet_id                 = azurerm_subnet.internal[each.key].id
+  network_security_group_id = each.value.network_security_group_id
+}
